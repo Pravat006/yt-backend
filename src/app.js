b7cb58d3e9572b5abae3db17de5b935aa5d77996
Pravat <beherapravat301@gmail.com>
2024-09-07 01:04:56 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -1,25 +1,31 @@
-import express from 'express';
-import cors from 'cors';
-import cookieParser from 'cookie-parser';
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
-
-app.use(cors({
+app.use(
+  cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true,
+  })
+);
 
-}));
-
-app.use(express.json({
-    limit: "16kb"
-}));
-app.use(express.urlencoded({extended: true, limit: "16kb"}));
+app.use(
+  express.json({
+    limit: "16kb",
+  })
+);
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 
 app.use(express.static("public"));
 
 app.use(cookieParser());
 
+//routes import
+import userRouter from "./routes/user.routes.js";
+//routes declaration
+
+app.use("/api/v1/users", userRouter);
 
 export default app;
-
---
+++ src/controllers/user.controller.js
@@ -0,0 +1,25 @@
+import { asyncHandler } from "../utils/asyncHandler.js";
+
+
+const registerUser= asyncHandler( async (req, res)=>{
+    res.status(200).json({
+        message: "on user register page",
+    });
+})
+
+
+
+export {
+    registerUser,
+    }
+
+
+
+
+
+
+
+
+
+
+

964cae442f6494f5e07583bb12e57186de694e26
Pravat <beherapravat301@gmail.com>
2024-09-12 20:49:25 +0530

    add refresh token endpoint

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -5,7 +5,7 @@ import jwt from 'jsonwebtoken'
 
 export const verifyJWT= asyncHandler(async(req, _ , next)=>{
     try {
-        const token= req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer","")
+        const token= req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ","")
     
         if(!token){
             throw new ApiError(401, "UnAuthorized token")
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loginUser, registerUser } from "../controllers/user.controller.js";
+import { loginUser, refreshAccessToken, registerUser } from "../controllers/user.controller.js";
 import {upload} from '../middlewares/multer.middleware.js'
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -15,7 +15,6 @@ router.route("/register").post(
         {
             name: "coverImage",
             maxCount: 1
-
         }
     ]),
     registerUser,
@@ -25,7 +24,7 @@ router.route("/login").post(loginUser)
 
 //securedd routes
 router.route("/logout").post(verifyJWT,loginUser)
-
+router.route("/refresh-token").post(refreshAccessToken)
 
 
 export default router
